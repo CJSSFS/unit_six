5490ad32446121b08203449af10ab2a677d05f3a
CJ <cj@iMac7.local>
2018-10-23 10:14:49 -0400

    Asked the user for a letter

--- assignment_six.py
+++ assignment_six.py
@@ -1,6 +1,11 @@
 import random
 
 
+def get_word():
+    word = ["Harry", "Ron", "Hermione", "Ginny", "Fred", "Neville", "Draco"]
+    my_word = random.choice(word)
+    return my_word
+
 def play():
     play1 = input("Would you like to play Squeal of Fortune?")
     if play1 == "y" or play1 == "yes":
@@ -9,5 +14,19 @@ def play():
         return False
 
 
-def letter():
-    letter1 = input("Please guess a letter?")
+def main():
+    answer = get_word()
+    letter = input("Please guess a letter:")
+    word = []
+    for x in range(len(answer)):
+        word.append("_")
+
+
+main()
+
+
+
+
+
+
+
