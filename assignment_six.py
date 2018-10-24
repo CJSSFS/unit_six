42bba07a967bceadfb4621b1cfbce1ca46ca6732
CJ <cj@iMac7.local>
2018-10-24 11:11:02 -0400

    Random word got guessed letter in word

--- assignment_six.py
+++ assignment_six.py
@@ -1,11 +1,6 @@
 import random
 
 
-def get_word():
-    word = ["Harry", "Ron", "Hermione", "Ginny", "Fred", "Neville", "Draco"]
-    my_word = random.choice(word)
-    return my_word
-
 def play():
     play1 = input("Would you like to play Squeal of Fortune?")
     if play1 == "y" or play1 == "yes":
@@ -14,12 +9,29 @@ def play():
         return False
 
 
+def get_word():
+    word = ["harry", "ron", "hermione", "ginny", "fred", "neville", "draco"]
+    my_word = random.choice(word)
+    return my_word
+
+
 def main():
+    get_word()
     answer = get_word()
-    letter = input("Please guess a letter:")
     word = []
     for x in range(len(answer)):
         word.append("_")
+    print(word)
+    while True:
+        letter = input("Please guess a letter:")
+        for x in range(len(answer)):
+            if letter == answer[x]:
+                word[x] = letter
+        print(word)
+
+        if answer == word:
+            print("You solved it!!!")
+
 
 
 main()
