6e5bc37d5e5f11a3e03f2ea466e4720d906ea150
CJ <cj@iMac7.local>
2018-10-25 12:04:56 -0400

    Added guesses remaining

--- assignment_six.py
+++ assignment_six.py
@@ -19,19 +19,28 @@ def main():
     get_word()
     answer = get_word()
     word = []
+    attempts = 6
+    letters_guessed = []
     for x in range(len(answer)):
         word.append("_")
     print(word)
     while True:
+        if attempts == 0 or "_" not in word:
+            break
+        print("You have",  attempts,  "remaining")
         letter = input("Please guess a letter:")
-        for x in range(len(answer)):
-            if letter == answer[x]:
-                word[x] = letter
+        if letter in answer:
+            for x in range(len(answer)):
+                if letter == answer[x]:
+                    word[x] = letter
+        else:
+            attempts = attempts - 1
+            print("That letter is not in the word")
         print(word)
+        print(letters_guessed)
 
         if answer == word:
             print("You solved it!!!")
-
 
 
 main()
